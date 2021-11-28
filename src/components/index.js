863ccf31067bdaf969a036a9391efd63af12883c
Aleksandra <olaszczur93@gmail.com>
2021-11-28 15:16:15 +0100

    fix: index.js update

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,1 @@
 export { default } from "./Counter";
-export { default as Button } from "./Button/Button";
-export { default as Input } from "./Input/Input";
-export { default as Number } from "./Number/Number";
